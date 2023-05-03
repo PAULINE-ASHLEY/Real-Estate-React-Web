d4d41b1996ff483faafadf1bec07207f142698f2
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2023-05-03 15:56:23 +0300

    I have created pages

---
+++ real-estate-react-web/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './HomePage';
+export { default as Buy } from './BuyPage';
+export { default as Rent } from './RentPage';
+export { default as Contact } from './ContactPage';
